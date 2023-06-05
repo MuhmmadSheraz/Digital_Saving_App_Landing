3ede33560fe8128510e52ea40b720f08c4a065a2
muhmmadsheraz <mshazy999@gmail.com>
2023-06-05 10:43:51 +0500

    replaced with vercel

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+      <div className="App-header">
+        <h2>Digital Saving App</h2>
+        <h3>Coming Soon...</h3>
+      </div>
+  );
+}
+
+export default App;
